d7a84d596b06566db57f4799e0ff5edc2b9026a3
hunderboy <wheeing@naver.com>
2020-11-09 16:43:43 +0900

    // 인트로 로그인 화면 구성중

---
+++ app/src/main/java/com/leesh/todolist/SelectLoginOrJoinActivity.kt
@@ -0,0 +1,12 @@
+package com.leesh.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectLoginOrJoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_login_or_join)
+    }
+}
