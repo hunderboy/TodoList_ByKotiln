f7e1cee8ebea4c6aa5f05ffb5868ba259e7c25c1
hunderboy <wheeing@naver.com>
2020-11-12 14:36:46 +0900

    // ViewPager2 적용중

---
+++ app/src/main/java/com/leesh/todolist/ViewPagerVersion2Activity.kt
@@ -0,0 +1,17 @@
+package com.leesh.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPagerVersion2Activity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager_version2)
+
+
+    }
+
+
+}
