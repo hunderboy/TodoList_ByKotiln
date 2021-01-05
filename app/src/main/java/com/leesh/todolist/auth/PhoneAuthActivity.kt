9f849f84381b1c461bf564a7e862c50dc3600b56
hunderboy <wheeing@naver.com>
2021-01-05 15:07:30 +0900

    // PhoneAuthActivity 패키지 이동

--- app/src/main/java/com/leesh/todolist/auth/PhoneAuthActivity.kt
+++ app/src/main/java/com/leesh/todolist/auth/PhoneAuthActivity.kt
@@ -1,21 +1,12 @@
-package com.leesh.todolist.custom
+package com.leesh.todolist.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.leesh.todolist.R
 
 class PhoneAuthActivity : AppCompatActivity() {
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_auth)
-
-
     }
-
-
-
 }
